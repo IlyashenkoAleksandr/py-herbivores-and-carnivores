28715c6ec8029bf502d01d54c694c83951ae5fbd
Oleksandr Iliashenko <21ilyashenko21@gmail.com>
2026-03-27 10:11:20 +0200

    refactor: fix type hints and PEP8 style for Animal classesÐ

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+
 class Animal:
-    alive = []
+    alive: list[Animal] = []
 
     def __init__(self,
                  name: str,
@@ -29,3 +32,5 @@ class Carnivore(Animal):
             other.health -= 50
         if other.health <= 0 and other in Animal.alive:
             Animal.alive.remove(other)
+
+g
